bb27db0756d2b08c6ec8bffc23cf615d0c90863c
Alex <alex.chernyak@gmail.com>
2021-03-14 14:40:36 -0700

    added manual instantiation as a baseline

--- Benchmarks/Program.cs
+++ Benchmarks/Program.cs
@@ -14,14 +14,6 @@ namespace Benchmarks
         }
     }
 
-    /// <summary>
-    /// |                          Method |         Mean |       Error |      StdDev |  Ratio | RatioSD |  Gen 0 |  Gen 1 | Gen 2 | Allocated |
-    /// |-------------------------------- |-------------:|------------:|------------:|-------:|--------:|-------:|-------:|------:|----------:|
-    /// |                  CreateInstance |   1,015.2 ns |    19.37 ns |    21.53 ns |   1.00 |    0.00 | 0.1297 |      - |     - |     272 B |
-    /// |                   CreateFactory | 406,110.2 ns | 7,775.55 ns | 6,892.82 ns | 401.82 |    9.19 | 2.9297 | 1.4648 |     - |    6545 B |
-    /// |            CreateFactoryGeneric | 404,532.1 ns | 7,827.73 ns | 8,375.59 ns | 398.65 |   11.74 | 2.9297 | 1.4648 |     - |    6601 B |
-    /// | CreateInstanceFromCachedFactory |     114.5 ns |     2.29 ns |     4.97 ns |   0.11 |    0.01 | 0.0725 |      - |     - |     152 B |
-    /// </summary>
     [MemoryDiagnoser]
     public class ActivatorHelperBenchmark
     {
@@ -33,10 +25,16 @@ namespace Benchmarks
             ActivatorUtilities.CreateFactory(typeof(CustomClass), new Type[] { typeof(string) });
 
         [Benchmark(Baseline = true)]
+        public void CreateInstanceManually()
+        {
+            var svc = _sp.GetService<RegisteredClass1>();
+            CustomClass instance = new CustomClass(svc, nameof(CreateInstanceManually));
+        }
+
+        [Benchmark]
         public void CreateInstance()
         {
             CustomClass instance = ActivatorUtilities.CreateInstance<CustomClass>(_sp, new[] { nameof(CreateInstance) });
-            string c = "a" + instance.Value;
         }
 
         [Benchmark]
@@ -44,21 +42,18 @@ namespace Benchmarks
         {
             var factory = ActivatorUtilities.CreateFactory(typeof(CustomClass), new Type[] { typeof(string) });
             CustomClass instance = factory.Invoke(_sp, new object[] { nameof(CreateFactory) }) as CustomClass;
-            string c = "a" + instance.Value;
         }
 
         [Benchmark]
         public void CreateFactoryGeneric()
         {
             CustomClass instance = CreateGenericFactory<CustomClass>(nameof(CreateFactoryGeneric));
-            string c = "a" + instance.Value;
         }
 
         [Benchmark]
         public void CreateInstanceFromCachedFactory()
         {
             CustomClass instance = _cachedFactory.Invoke(_sp, new object[] { nameof(CreateInstanceFromCachedFactory) }) as CustomClass;
-            string c = "a" + instance.Value;
         }
 
         private T CreateGenericFactory<T>(params object[] runtimeParams) where T: CustomClass
